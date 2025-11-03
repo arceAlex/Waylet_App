e4bd36feb1f8eb1f30bcb28d49aea2f33ee0a749
alex <alex.arce.leon@gmail.com>
2025-11-03 12:54:19 +0100

    delete comments

--- Waylet_App/CommerceDetail/View/CommerceDetailViewController.swift
+++ Waylet_App/CommerceDetail/View/CommerceDetailViewController.swift
@@ -6,33 +6,6 @@
 //
 
 import UIKit
-
-//class CommerceDetailViewController: UIViewController {
-//    
-//    var commerce: Commerce?
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        self.title = commerce?.name
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
-
-import UIKit
 import MapKit
 
 class CommerceDetailViewController: UIViewController {
